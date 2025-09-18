7f129f41abee19745b5c92efb8186b311b1fac4d
Rizqi Siti Inayah 2428250080P <rizqisitiinayah_2428250080p@mhs.mdp.ac.id>
2025-09-18 18:41:42 +0700

    push 2

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,20 +5,62 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Exception;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login (LoginRequest $request) {
+    public function login (LoginRequest $request) 
+    {
+        try {
+            $validate = $request->safe()->all();
 
+            if (!Auth::attempt($validated)) {
+                return response()->json([
+                    'message' => "Email atau password salah",
+                    'data' => null
+                ], 401);
+            }
+            $user = $request->user();
+            $token = $user->createToken('laravel_api', ['*'])->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login Berhasil',
+                'user' => $user,
+                'token' => $token
+            ], 200);
+        }catch(Exception $e) {
+            return response()->json([
+                    'message' => $e->getMessage(),
+                    'data' => null
+                ], 500);
+        }
     }
 
     public function register (RegisterRequest $request) {
         try {
             $validated = $request->safe()->all();
-        }catch(Exception $e) {
 
+            $passwordHash = Hash::make($validated['password']); //untuk mengenkripsi password
+
+            $validated['password'] = $passwordHash;
+
+            $response = User::create($validated); //membuat user baru
+
+            if($response){
+                return response()->json([
+                    'message' => 'register berhasil',
+                    'data' => null
+                ], 201); //http code 201, berhasil membuat kode artinya
+            }
+        }catch(Exception $e) {
+            return response()->json([
+                    'message' => $e->getMessage(),
+                    'data' => null
+                ], 500);
         }
 
     }
